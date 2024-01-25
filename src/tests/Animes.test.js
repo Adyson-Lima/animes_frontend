ce6b1abbe16222382cd372f2241dbf93869e89be
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-25 09:39:20 -0300

    6° commit - tdd tela Animes: Existe tabela em Animes? RED

--- src/tests/Animes.test.js
+++ src/tests/Animes.test.js
@@ -20,4 +20,8 @@ describe('Testes da tela Animes', () => {
     expect(screen.getByTestId('mylink')).toBeInTheDocument();
   });
 
+  it('Existe tabela em Animes?', () => {
+    expect(screen.getByTestId('mytable')).toBeInTheDocument();
+  });
+
 });
